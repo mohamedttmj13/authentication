d3f014fca58d19d3b607e101df59afabbbbc2db1
mohamedttmj13 <mohamedtt_mj13@yahoo.com.augit config --replace-all user.name>
2022-02-19 11:18:11 +1030

    second commit

--- app.js
+++ app.js
@@ -328,4 +328,6 @@ app.get('/singOut', (req, res)=>{
 
 //----------------------------END ROUTES--------------------------------------------
 
-app.listen(process.env.PORT || 8000, ()=>console.log("The app is running on port 8000"));
+app.listen(process.env.PORT || 3000, function(){
+    console.log("The app is running on port 8000");
+});
